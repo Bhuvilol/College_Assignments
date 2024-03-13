c379fa07906d534345ccb5ac053c431e47673a1f
Bhabesh <147243844+Bhuvilol@users.noreply.github.com>
2024-03-13 21:34:34 +0530

    Add files via upload

---
+++ Sem1/Assignment1/A1Q2.java
@@ -0,0 +1,11 @@
+
+public class A1Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*******     ******");
+		System.out.println("      *     *    *");
+		System.out.println("      *******    *******");
+	}
+
+}
