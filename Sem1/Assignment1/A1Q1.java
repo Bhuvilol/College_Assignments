362c6fe9e2d79d5b9d798fa201a86c94924c19a2
Bhabesh <147243844+Bhuvilol@users.noreply.github.com>
2024-03-13 21:33:49 +0530

    Add files via upload

---
+++ Sem1/Assignment1/A1Q1.java
@@ -0,0 +1,12 @@
+
+public class A1Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello \"ITERIAN\"");
+		System.out.println("Welcome to Siksha 'O' Anusandhan Family.");
+		System.out.println("Welcome to \"Introduction to Computer Laboratory\"");
+		System.out.println("Java is fun for All!!");
+	}
+
+}
