f215ba33ccb8255e433c56593dc924404bf0b645
Bhabesh <bhabeshcse@gmail.com>
2024-03-15 17:28:50 +0530

    Update Q7.java

--- Sem2/Assignment1/Q7.java
+++ Sem2/Assignment1/Q7.java
@@ -50,3 +50,12 @@ public class Q7 {
 	}
 
 }
+
+
+//output
+// Enter number of elements of Array: 5
+// Enter elements of the array: 12 14 12 14 13
+// Maximum element of Array is 14 and occurs 2 times.
+// Minimum element of Array is 12 and occurs 2 times.
+// First occurrence of maximum element is at position 2.
+// Last occurrence of minimum element is at position 3.
