5acfacac96671f8a222d705c78c6fcdb69fcbdcd
Bhabesh <bhabeshcse@gmail.com>
2024-03-15 17:24:48 +0530

    Update Q4.java

--- Sem2/Assignment1/Q4.java
+++ Sem2/Assignment1/Q4.java
@@ -3,37 +3,29 @@ public class Q4
 {
 
     public static void main(String[] args)
-
     {
-
         char ch[] = {'c', 'a', 'r'};
-
         for (int i = 0; i < ch.length; i++)
-
         {
-
             for (int j = 0; j < ch.length; j++) 
-
             {
-
                 for (int k = 0; k < ch.length; k++)
-
                 {
-
                     if (i != j && j != k && i != k)
-
                     {
-
                         System.out.println(ch[i] + "" + ch[j] + "" + ch[k]);
-
                     }
-
                 }
-
             }
-
         }
-
     }
-
 }
+
+//output
+
+// car
+// cra
+// acr
+// arc
+// rca
+// rac
