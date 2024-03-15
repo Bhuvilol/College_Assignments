b31bf7b45d1fa718fa4da4a9d5d9060209fd58c9
Bhabesh <bhabeshcse@gmail.com>
2024-03-15 17:19:00 +0530

    Add files via upload

---
+++ Sem2/Assignment1/HAQ5.java
@@ -0,0 +1,43 @@
+
+public class HAQ5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][] = new int[4][4];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				arr[i][j] = (int)(Math.random()+0.5);
+			}
+		}
+		System.out.println("Computer generated 4X4 matrix is: ");
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		int rowmax = Integer.MIN_VALUE,posR=0;
+		int colmax = Integer.MIN_VALUE,posC=0;
+		for (int i = 0; i < 4 ; i++) {
+			int rowsum =0;
+			int colsum =0;
+			for (int j = 0; j < 4 ; j++) {
+				rowsum += arr[i][j];
+			}
+			for (int j = 0; j < 4 ; j++) {
+				colsum += arr[j][i];
+			}
+			if (rowsum>rowmax) {
+				rowmax=rowsum;
+				posR=i;
+			}
+			if (colsum>colmax) {
+				colmax=colsum;
+				posC=i;
+			}
+		}
+		System.out.println("The largest row index:"+posR);
+		System.out.println("The largest col index:"+posC);
+	}
+
+}
