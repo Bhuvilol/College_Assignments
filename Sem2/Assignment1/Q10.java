3968ab09e32d895639491c5c12f171c4e9e36bec
Bhabesh <bhabeshcse@gmail.com>
2024-03-15 17:31:58 +0530

    Update Q10.java

--- Sem2/Assignment1/Q10.java
+++ Sem2/Assignment1/Q10.java
@@ -35,3 +35,13 @@ public class Q10 {
 	}
 
 }
+
+//output
+// Enter a 3-by-4 matrix row by row: 
+// 1.5 2 3 4
+// 5.5 6 7 8
+// 9.5 1 3 1
+// Sum of the elements at column 0 is 16.5
+// Sum of the elements at column 1 is 9.0
+// Sum of the elements at column 2 is 13.0
+// Sum of the elements at column 3 is 13.0
