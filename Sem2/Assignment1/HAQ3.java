7b185f8f7eabb4a025a06caf20518be095e91554
Bhabesh <bhabeshcse@gmail.com>
2024-03-15 17:18:14 +0530

    Add files via upload

---
+++ Sem2/Assignment1/HAQ3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HAQ3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the length of arrays");
+		int n = sc.nextInt();
+		int arrA[] = new int[n];
+		System.out.println("Enter the contents of Array A");
+		for (int i = 0; i < arrA.length; i++) {
+			arrA[i]=sc.nextInt();
+		}
+		int arrB[] = new int[n];
+		System.out.println("Enter the contents of Array B");
+		for (int i = 0; i < arrB.length; i++) {
+			arrB[i]=sc.nextInt();
+		}
+		int arrC[] = new int[n];
+		System.out.println("The contents of Array C are");
+		for (int i = 0; i < arrC.length; i++) {
+			arrC[i]= arrA[i]*arrB[i];
+		}
+		for (int i : arrC) {
+			System.out.println(i+" ");
+		}
+		
+	}
+
+}
