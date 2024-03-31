b84fb6d3bd743ae6f35798f33ea22678b755606d
Bhabesh <bhabeshcse@gmail.com>
2024-03-31 21:07:32 +0530

    Add files via upload

---
+++ Sem2/Assignment2/Q7.java
@@ -0,0 +1,32 @@
+interface DetailInfo{
+	void display();
+	int count();
+}
+class people implements DetailInfo{
+	static int maxcount;
+	String name;
+	people(String name) {
+		this.name = name;
+	}
+	public void display() {
+		System.out.println("Name of person = "+name);
+		System.out.println("No. of character name comtains = "+maxcount);
+	}
+	public int count() {
+		maxcount = name.length();
+		return maxcount;
+	}
+}
+public class Q7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		people p1 = new people("Bhabesh");
+		p1.count();
+		p1.display();
+		people p2 = new people("Nibedita");
+		p2.count();
+		p2.display();
+	}
+
+}
