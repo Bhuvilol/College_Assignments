07977052256097dff9533209a4a6de80c513487a
Bhabesh <bhabeshcse@gmail.com>
2024-03-31 20:34:25 +0530

    Add files via upload

---
+++ Sem2/Assignment2/Q6.java
@@ -0,0 +1,43 @@
+abstract class Marks{
+	int markICP;
+	int markDSA;
+	double percentage;
+	abstract double getPercentage();
+}
+class CSE extends Marks{
+	int algoDesign;
+	CSE(int a, int b, int c){
+		markICP = a;
+		markDSA = b;
+		algoDesign = c;
+	}
+	double getPercentage(){
+		percentage = ((markICP+markDSA+algoDesign)/3);
+		return percentage ;
+	}
+}
+class NonCSE extends Marks{
+	int enggMechanics;
+	NonCSE(int a, int b, int c){
+		markICP = a;
+		markDSA = b;
+		enggMechanics = c;
+	}
+	double getPercentage(){
+		percentage = (1.0*(markICP+markDSA+enggMechanics)/3) ;
+		return percentage;
+	}
+}
+public class Q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CSE s1 = new CSE(70,90,88);
+		s1.getPercentage();
+		System.out.println("Percentage secured of student 1 : "+s1.percentage);
+		NonCSE s2 = new NonCSE(80,69,90);
+		s2.getPercentage();
+		System.out.println("Percentage secured of student 2 : "+s2.percentage);
+	}
+
+}
