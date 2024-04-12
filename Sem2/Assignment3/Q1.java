e97991a1373744ea9c742d071a2180c4368d52cd
Bhabesh <bhabeshcse@gmail.com>
2024-04-12 20:49:03 +0530

    Update Q1.java

--- Sem2/Assignment3/Q1.java
+++ Sem2/Assignment3/Q1.java
@@ -6,17 +6,28 @@ public class Q1 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		try {
-            System.out.print("Enter your lucky number: ");
-            int luckyNumber = sc.nextInt();
-            if (luckyNumber < 0) {
-                throw new NumberFormatException("Negative number not allowed");
-            }
-            System.out.println("Your lucky number is: " + luckyNumber);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        } finally {
-            sc.close();
-        }
+            		System.out.print("Enter your lucky number: ");
+            		int luckyNumber = sc.nextInt();
+            		if (luckyNumber < 0) {
+               			throw new NumberFormatException("Negative number not allowed");
+            		}
+            		System.out.println("Your lucky number is: " + luckyNumber);
+        	} catch (NumberFormatException e) {
+            		System.out.println(e);
+        	} finally {
+            		sc.close();
+        	}
 	}
 
 }
+
+
+
+
+
+
+
+
+//output
+// Enter your lucky number: -90
+// java.lang.NumberFormatException: Negative number 
