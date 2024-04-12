e5581589ab868494d7484e0109938b1882a10748
Bhabesh <bhabeshcse@gmail.com>
2024-04-12 20:00:45 +0530

    Update HAQ5.java

--- Sem2/Assignment2/HAQ5.java
+++ Sem2/Assignment2/HAQ5.java
@@ -43,6 +43,9 @@ public class HAQ5 {
 		PointType pt1 = new PointType(3, 4);
 		System.out.println("The coordinates of the point are :");
 		pt1.show();
+		pt1.setXY(5, 6);
+		System.out.println("The coordinates of the new point are :");
+		pt1.show();
 		CircleType c1 = new CircleType(2, 3);
 		c1.setRadius(5);
 		System.out.println("The position of the circle is : ( "+c1.getX()+", "+c1.getY()+" )");
@@ -54,3 +57,17 @@ public class HAQ5 {
 	}
 
 }
+
+
+
+
+
+//output
+// The coordinates of the point are :
+// x = 3.0 y = 4.0
+// The coordinates of the new point are :
+// x = 5.0 y = 6.0
+// The position of the circle is : ( 2.0, 3.0 )
+// Radius of circle = 5.0
+// Area of circle = 78.53981633974483
+// Circumference of circle = 31.41592653589793
