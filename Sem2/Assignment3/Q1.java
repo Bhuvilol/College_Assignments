507321d5477eab53902b0629615cafd376cabdd2
Bhabesh <bhabeshcse@gmail.com>
2024-04-12 20:46:53 +0530

    Add files via upload

---
+++ Sem2/Assignment3/Q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		try {
+            System.out.print("Enter your lucky number: ");
+            int luckyNumber = sc.nextInt();
+            if (luckyNumber < 0) {
+                throw new NumberFormatException("Negative number not allowed");
+            }
+            System.out.println("Your lucky number is: " + luckyNumber);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        } finally {
+            sc.close();
+        }
+	}
+
+}
