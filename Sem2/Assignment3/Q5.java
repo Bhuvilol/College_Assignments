96116fdb7dd4adc2855102955ca12213984e604d
Bhabesh <bhabeshcse@gmail.com>
2024-06-08 06:29:31 +0530

    Add files via upload

---
+++ Sem2/Assignment3/Q5.java
@@ -0,0 +1,22 @@
+
+public class Q5 {
+	public static <E> void printArray(E[] inputArray) {
+		for (E e : inputArray) {
+			System.out.print(e+" ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] iar = {1,2,3,4,5};
+		printArray(iar);
+		
+		String[] sar = {"liku","liza","bhuvi"};
+		printArray(sar);
+		
+		Double[] dar = {1.1,2.2,3.3};
+		printArray(dar);
+	}
+
+}
