cd196757fa40e8cbcea4f724325cf6d051b87e47
Bhabesh <bhabeshcse@gmail.com>
2024-09-15 15:44:22 +0530

    Add files via upload

---
+++ Sem3/AD_Assgn1/Q3_Method1.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q3_Method1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr= {10,20,30,40,50,60};
+		System.out.print("Before rotation : ");
+		System.out.println(Arrays.toString(arr));
+		
+		System.out.print("Enter the position to rotate by : ");
+		int k = sc.nextInt();
+		
+		int[] narr = new int[arr.length];
+		int p =0;
+		for (int i = k; i < narr.length; i++) {
+			narr[i]=arr[p++];
+		}
+		int pos = k;
+		for (int i = 0; i < k; i++) {
+			narr[i]=arr[p++];
+		}
+		
+		
+		System.out.print("After rotation : ");
+		System.out.println(Arrays.toString(narr));
+	}
+
+}
